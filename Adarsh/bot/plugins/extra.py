458fe892a42ccbaad06265d505e84e1dea5e049b
Sourcery AI <>
2023-03-22 16:17:32 +0000

    'Refactored by Sourcery'

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -13,41 +13,44 @@ START_TEXT = """ Your Telegram DC Is : `{}`  """
 @StreamBot.on_message(filters.regex("maintainers😎"))
 async def maintainers(b,m):
     try:
-       await b.send_message(chat_id=m.chat.id,text="HELLO",quote=True)
+        await b.send_message(chat_id=m.chat.id,text="HELLO",quote=True)
     except Exception:
-                await b.send_message(
-                    chat_id=m.chat.id,
-                    text="I am Made By [GreyMatter's Bot](https://t.me/GreyMatter_Bots)",
-                    
-                    reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton("Owner💻", url=f"https://t.me/GreyMatter_Owner")
-                            ]
-                        ]
-                    ),
-                    
-                    disable_web_page_preview=True)
+        await b.send_message(
+            chat_id=m.chat.id,
+            text="I am Made By [GreyMatter's Bot](https://t.me/GreyMatter_Bots)",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "Owner💻", url="https://t.me/GreyMatter_Owner"
+                        )
+                    ]
+                ]
+            ),
+            disable_web_page_preview=True,
+        )
             
          
 @StreamBot.on_message(filters.regex("Subscribe ❤️"))
 async def follow_user(b,m):
     try:
-       await b.send_message(chat_id=m.chat.id,text="HELLO",quote=True)
+        await b.send_message(chat_id=m.chat.id,text="HELLO",quote=True)
     except Exception:
-                await b.send_message(
-                    chat_id=m.chat.id,
-                    text="<B>HERE'S THE SUBSCRIBE LINK</B>",
-                    
-                    reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton("Subscribe ❤️", url=f"https://youtube.com/c/GreyMattersBot")
-                            ]
-                        ]
-                    ),
-                    
-                    disable_web_page_preview=True)
+        await b.send_message(
+            chat_id=m.chat.id,
+            text="<B>HERE'S THE SUBSCRIBE LINK</B>",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "Subscribe ❤️",
+                            url="https://youtube.com/c/GreyMattersBot",
+                        )
+                    ]
+                ]
+            ),
+            disable_web_page_preview=True,
+        )
         
 
 @StreamBot.on_message(filters.regex("DC"))
